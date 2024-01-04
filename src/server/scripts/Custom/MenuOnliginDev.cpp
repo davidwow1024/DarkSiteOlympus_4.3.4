a8a5b73949f0f9b0ed7c364e2b0a90e14f545019
RëXol <66186993+jorge990125@users.noreply.github.com>
2024-01-04 15:47:36 -0500

    Update MenuOnliginDev.cpp

--- src/server/scripts/Custom/MenuOnliginDev.cpp
+++ src/server/scripts/Custom/MenuOnliginDev.cpp
@@ -16,10 +16,12 @@ public:
 		player->ADD_GOSSIP_ITEM(4, " |cFF9400D3Recuerde cumplir con las normas establecidas", GOSSIP_SENDER_MAIN, 999);
 		player->ADD_GOSSIP_ITEM(4, " |cFF9400D3El servidor consta con:", GOSSIP_SENDER_MAIN, 999);
 	    player->ADD_GOSSIP_ITEM(4, " |cFF9400D3Anti Farming", GOSSIP_SENDER_MAIN, 999);
-		player->ADD_GOSSIP_ITEM(4, " |cFF9400D3Rates de experiencia x10", GOSSIP_SENDER_MAIN, 999);
+		player->ADD_GOSSIP_ITEM(4, " |cFF9400D3Rates de experiencia x5", GOSSIP_SENDER_MAIN, 999);
+		player->ADD_GOSSIP_ITEM(4, " |cFF9400D3Rates de honor x3", GOSSIP_SENDER_MAIN, 999);
 		player->ADD_GOSSIP_ITEM(4, " |cFF9400D3Rates de profesiones x2", GOSSIP_SENDER_MAIN, 999);
+		player->ADD_GOSSIP_ITEM(4, " |cFF9400D3Rates de oro x2", GOSSIP_SENDER_MAIN, 999);
 		player->ADD_GOSSIP_ITEM(4, " |cFF9400D3Contenido Blizzlike", GOSSIP_SENDER_MAIN, 999);
-		player->ADD_GOSSIP_ITEM(4, " |cFF9400D3Bandas y Mazmorras parche 4.3.4 funcionales", GOSSIP_SENDER_MAIN, 999);
+		player->ADD_GOSSIP_ITEM(4, " |cFF9400D3Bandas y Mazmorras parche 4.0.6 funcionales", GOSSIP_SENDER_MAIN, 999);
 		player->ADD_GOSSIP_ITEM(4, " |cFF8B0000---------------------------------------", GOSSIP_SENDER_MAIN, 999);
 		player->ADD_GOSSIP_ITEM(4, "    |cFF006400Anticheat On |cFF8B0000| |cFF006400Secure System On", GOSSIP_SENDER_MAIN, 999);
                                                                             // SetMenuId must be after clear menu and before send menu!!
