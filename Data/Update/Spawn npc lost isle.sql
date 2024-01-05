258da1f7220d60c6eb611ffb34c80c4087f0a62a
RëXol <66186993+jorge990125@users.noreply.github.com>
2024-01-05 16:51:12 -0500

    DB/Fix Spawn npc lost isle
    phase 10240
    phase 2048
    delete npc phase 4096

---
+++ Data/Update/Spawn npc lost isle.sql
@@ -0,0 +1,31 @@
+--delete creature--
+DELETE FROM `creature`  WHERE `guid` = 247521;
+DELETE FROM `creature`  WHERE `guid` = 246796;
+DELETE FROM `creature`  WHERE `guid` = 247486;
+DELETE FROM `creature`  WHERE `guid` = 247487;
+DELETE FROM `creature`  WHERE `guid` = 247566;
+DELETE FROM `creature`  WHERE `guid` = 247588;
+DELETE FROM `creature`  WHERE `guid` = 247484;
+DELETE FROM `creature`  WHERE `guid` = 247572;
+DELETE FROM `creature`  WHERE `guid` = 247533;
+DELETE FROM `creature`  WHERE `guid` = 247460;
+DELETE FROM `creature`  WHERE `guid` = 247575;
+DELETE FROM `creature`  WHERE `guid` = 247631;
+DELETE FROM `creature`  WHERE `guid` = 247532;
+DELETE FROM `creature`  WHERE `guid` = 247459;
+DELETE FROM `creature`  WHERE `guid` = 247531;
+DELETE FROM `creature`  WHERE `guid` = 247530;
+DELETE FROM `creature`  WHERE `guid` = 247528;
+DELETE FROM `creature`  WHERE `guid` = 247536;
+DELETE FROM `creature`  WHERE `guid` = 247574;
+DELETE FROM `creature`  WHERE `guid` = 247473;
+DELETE FROM `creature`  WHERE `guid` = 247535;
+DELETE FROM `creature`  WHERE `guid` = 247540;
+DELETE FROM `creature`  WHERE `guid` = 247539;
+DELETE FROM `creature`  WHERE `guid` = 247502;
+DELETE FROM `creature`  WHERE `guid` = 247537;
+DELETE FROM `creature`  WHERE `guid` = 247519;
+DELETE FROM `creature`  WHERE `guid` = 247538;
+
+--add creature--
+insert into `creature` (`guid`, `id`, `map`, `zone`, `area`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `walkmode`) values('326969','38120','648','4720','4870','1','10240','0','38120','919.889','2335.79','3.51271','3.00493','300','0','0','198','0','0','0','0','0','0');
