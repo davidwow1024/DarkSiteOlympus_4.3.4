f99d8ab5535075214a1cd9c538c629269d726e5d
RëXol <66186993+jorge990125@users.noreply.github.com>
2024-01-09 17:29:51 -0500

    Call to arms

---
+++ Data/UPDATE/call to arms.sql
@@ -0,0 +1,14 @@
+DELETE FROM `game_event` WHERE `eventEntry` = '128';
+DELETE FROM `game_event` WHERE `eventEntry` = '129';
+DELETE FROM `game_event` WHERE `eventEntry` = '130';
+DELETE FROM `game_event` WHERE `eventEntry` = '132';
+
+
+UPDATE `world`.`game_event` SET `eventEntry`='18', `start_time`='2019-12-31 16:00:00', `end_time`='2030-01-01 06:00:00', `occurence`='60480', `length`='6240', `holiday`='283', `holidayStage`='0', `description`='Call to Arms: Alterac Valley!', `world_event`='1' WHERE (`eventEntry`='18');
+UPDATE `world`.`game_event` SET `eventEntry`='20', `start_time`='2019-12-31 16:00:00', `end_time`='2030-01-01 06:00:00', `occurence`='60480', `length`='6240', `holiday`='285', `holidayStage`='0', `description`='Call to Arms: Arathi Basin!', `world_event`='1' WHERE (`eventEntry`='20');
+UPDATE `world`.`game_event` SET `eventEntry`='21', `start_time`='2019-12-31 16:00:00', `end_time`='2030-01-01 06:00:00', `occurence`='60480', `length`='6240', `holiday`='353', `holidayStage`='0', `description`='Call to Arms: Eye of the Storm!', `world_event`='1' WHERE (`eventEntry`='21');
+UPDATE `world`.`game_event` SET `eventEntry`='54', `start_time`='2019-12-31 16:00:00', `end_time`='2030-01-01 06:00:00', `occurence`='60480', `length`='6240', `holiday`='420', `holidayStage`='0', `description`='Call to Arms: Isle of Conquest!', `world_event`='1' WHERE (`eventEntry`='54');
+UPDATE `world`.`game_event` SET `eventEntry`='53', `start_time`='2019-12-31 16:00:00', `end_time`='2030-01-01 06:00:00', `occurence`='60480', `length`='6240', `holiday`='400', `holidayStage`='0', `description`='Call to Arms: Strand of the Ancients!', `world_event`='1' WHERE (`eventEntry`='53');
+UPDATE `world`.`game_event` SET `eventEntry`='76', `start_time`='2019-12-31 16:00:00', `end_time`='2030-01-01 06:00:00', `occurence`='60480', `length`='5758', `holiday`='435', `holidayStage`='0', `description`='Call to Arms: The Battle for Gilneas', `world_event`='2' WHERE (`eventEntry`='76');
+UPDATE `world`.`game_event` SET `eventEntry`='77', `start_time`='2019-12-31 16:00:00', `end_time`='2030-01-01 06:00:00', `occurence`='60640', `length`='5758', `holiday`='436', `holidayStage`='0', `description`='Call to Arms: Twin Peaks', `world_event`='1' WHERE (`eventEntry`='77');
+UPDATE `world`.`game_event` SET `eventEntry`='19', `start_time`='2019-12-31 16:00:00', `end_time`='2030-01-01 06:00:00', `occurence`='60480', `length`='6240', `holiday`='284', `holidayStage`='0', `description`='Call to Arms: Warsong Gulch!', `world_event`='1' WHERE (`eventEntry`='19');
