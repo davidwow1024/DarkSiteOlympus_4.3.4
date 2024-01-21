0e57b6bbc12412d93ebecb3b2abc5fd9c4415dd3
liosbel2000 <133018126+liosbel2000@users.noreply.github.com>
2024-01-21 22:38:45 +0300

    Create 21-01-24-World Update.sql

---
+++ Data/UPDATE/21-01-24-World Update.sql
@@ -0,0 +1,109 @@
+
+replace into `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `exp_unk`, `faction_a`, `faction_h`, `npcflag`, `speed_walk`, `speed_run`, `speed_swim`, `speed_fly`, `scale`, `rank`, `mindmg`, `maxdmg`, `dmgschool`, `attackpower`, `dmg_multiplier`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `minrangedmg`, `maxrangedmg`, `rangedattackpower`, `type`, `type_flags`, `type_flags2`, `lootid`, `pickpocketloot`, `skinloot`, `resistance1`, `resistance2`, `resistance3`, `resistance4`, `resistance5`, `resistance6`, `spell1`, `spell2`, `spell3`, `spell4`, `spell5`, `spell6`, `spell7`, `spell8`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `HoverHeight`, `Health_mod`, `Mana_mod`, `Mana_mod_extra`, `Armor_mod`, `RacialLeader`, `questItem1`, `questItem2`, `questItem3`, `questItem4`, `questItem5`, `questItem6`, `movementId`, `RegenHealth`, `equipment_id`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`, `WDBVerified`) values('39436','0','0','0','0','0','11553','0','0','0','Twilight Proveditor','','','0','80','80','3','0','2146','2146','0','1','1.14286','1','1.14286','1','0','464','604','0','708','1','2000','2000','1','512','2048','0','0','0','0','0','0','464','604','708','7','0','0','39436','39436','0','0','0','0','0','0','0','80182','0','0','0','0','0','0','0','0','0','2223','2223','SmartAI','0','1','0','1','1','1','1','0','0','0','0','0','0','0','0','1','0','0','0','','13623');
+
+DELETE FROM `creature` WHERE `id`=39436;
+
+insert into `creature` (`guid`, `id`, `map`, `zone`, `area`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `walkmode`) values('99998541','39436','1','616','4978','1','169','11553','0','5238.89','-2373.41','1432.72','0.350904','40','0','0','1','1','2','0','0','0','0');
+insert into `creature` (`guid`, `id`, `map`, `zone`, `area`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `walkmode`) values('99998540','39436','1','616','4978','1','169','11553','0','5359.12','-2288.91','1440.17','0.17528','40','0','0','1','1','2','0','0','0','0');
+insert into `creature` (`guid`, `id`, `map`, `zone`, `area`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `walkmode`) values('99998539','39436','1','616','4978','1','169','11553','0','5437.11','-2290.15','1456.92','5.716','40','0','0','1','1','2','0','0','0','0');
+insert into `creature` (`guid`, `id`, `map`, `zone`, `area`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `walkmode`) values('99998538','39436','1','616','4978','1','169','11553','0','5551.24','-2469.3','1475.1','4.95282','40','0','0','1','1','2','0','0','0','0');
+insert into `creature` (`guid`, `id`, `map`, `zone`, `area`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `walkmode`) values('99998537','39436','1','616','4978','1','169','11553','0','5190.34','-2384.48','1427.76','6.16113','40','0','0','1','1','2','0','0','0','0');
+insert into `creature` (`guid`, `id`, `map`, `zone`, `area`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `walkmode`) values('99998536','39436','1','616','4978','1','65535','0','0','5126.27','-2350.27','1413.82','5.36566','40','0','0','30951','0','2','0','0','0','0');
+insert into `creature` (`guid`, `id`, `map`, `zone`, `area`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `walkmode`) values('99998535','39436','1','616','4978','1','169','11553','0','5517.73','-2408.7','1467.47','5.30463','40','0','0','1','1','2','0','0','0','0');
+insert into `creature` (`guid`, `id`, `map`, `zone`, `area`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `walkmode`) values('99998534','39436','1','616','4978','1','169','11553','0','5545.04','-2560.83','1479.94','4.50396','40','0','0','1','1','2','0','0','0','0');
+insert into `creature` (`guid`, `id`, `map`, `zone`, `area`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `walkmode`) values('99998533','39436','1','616','4978','1','169','11553','0','5483.24','-2351.26','1462.17','5.04375','40','0','0','1','1','2','0','0','0','0');
+insert into `creature` (`guid`, `id`, `map`, `zone`, `area`, `spawnMask`, `phaseMask`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `unit_flags`, `dynamicflags`, `walkmode`) values('99998542','39436','1','616','4978','1','169','11553','0','5293.29','-2315.83','1433.29','0.436206','40','0','0','1','1','2','0','0','0','0');
+
+REPLACE INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES('127427','1774260','0','0','0','0',NULL);
+REPLACE INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES('127577','1774260','0','0','0','0',NULL);
+REPLACE INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES('127529','1774260','0','0','0','0',NULL);
+REPLACE INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES('127506','1774260','0','0','0','0',NULL);
+REPLACE INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES('127498','1774260','0','0','0','0',NULL);
+REPLACE INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES('110014','1774260','0','0','0','0',NULL);
+REPLACE INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES('109910','1774260','0','0','0','0',NULL);
+REPLACE INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES('109827','1774260','0','0','0','0',NULL);
+REPLACE INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES('109764','1774260','0','0','0','0',NULL);
+REPLACE INTO `creature_addon` (`guid`, `path_id`, `mount`, `bytes1`, `bytes2`, `emote`, `auras`) VALUES('127445','1774260','0','0','0','0',NULL);
+
+UPDATE `creature` SET `guid`=127427 WHERE `guid`=99998533;
+UPDATE `creature` SET `guid`=127577 WHERE `guid`=99998534;
+UPDATE `creature` SET `guid`=127529 WHERE `guid`=99998535;
+UPDATE `creature` SET `guid`=127498 WHERE `guid`=99998536;
+UPDATE `creature` SET `guid`=127506 WHERE `guid`=99998537;
+UPDATE `creature` SET `guid`=110014 WHERE `guid`=99998538;
+UPDATE `creature` SET `guid`=109910 WHERE `guid`=99998539;
+UPDATE `creature` SET `guid`=109827 WHERE `guid`=99998540;
+UPDATE `creature` SET `guid`=109764 WHERE `guid`=99998541;
+UPDATE `creature` SET `guid`=127445 WHERE `guid`=99998542;
+
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `action`, `action_chance`, `inverse_formation_angle`, `wpguid`, `move_flag`) values('1774260','14','5585.21','-2823.24','1515.15','0','0','0','100','0','0','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `action`, `action_chance`, `inverse_formation_angle`, `wpguid`, `move_flag`) values('1774260','13','5605.06','-2776.64','1509.26','0','0','0','100','0','0','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `action`, `action_chance`, `inverse_formation_angle`, `wpguid`, `move_flag`) values('1774260','12','5582.78','-2750.16','1502.04','0','0','0','100','0','0','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `action`, `action_chance`, `inverse_formation_angle`, `wpguid`, `move_flag`) values('1774260','11','5573.36','-2696.14','1491.48','0','0','0','100','0','0','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `action`, `action_chance`, `inverse_formation_angle`, `wpguid`, `move_flag`) values('1774260','10','5536.67','-2637.04','1488.5','0','0','0','100','0','0','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `action`, `action_chance`, `inverse_formation_angle`, `wpguid`, `move_flag`) values('1774260','9','5531.42','-2596.05','1480.79','0','0','0','100','0','0','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `action`, `action_chance`, `inverse_formation_angle`, `wpguid`, `move_flag`) values('1774260','8','5545.04','-2560.83','1479.94','0','0','0','100','0','0','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `action`, `action_chance`, `inverse_formation_angle`, `wpguid`, `move_flag`) values('1774260','7','5550.86','-2511.27','1479.01','0','0','0','100','0','0','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `action`, `action_chance`, `inverse_formation_angle`, `wpguid`, `move_flag`) values('1774260','6','5551.24','-2469.3','1475.1','0','0','0','100','0','0','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `action`, `action_chance`, `inverse_formation_angle`, `wpguid`, `move_flag`) values('1774260','5','5539.54','-2429.97','1470.31','0','0','0','100','0','0','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `action`, `action_chance`, `inverse_formation_angle`, `wpguid`, `move_flag`) values('1774260','4','5518.18','-2409.37','1467.55','0','0','0','100','0','0','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `action`, `action_chance`, `inverse_formation_angle`, `wpguid`, `move_flag`) values('1774260','3','5517.73','-2408.7','1467.47','0','0','0','100','0','0','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `action`, `action_chance`, `inverse_formation_angle`, `wpguid`, `move_flag`) values('1774260','2','5488.93','-2372.6','1463.23','0','0','0','100','0','0','0');
+insert into `waypoint_data` (`id`, `point`, `position_x`, `position_y`, `position_z`, `orientation`, `delay`, `action`, `action_chance`, `inverse_formation_angle`, `wpguid`, `move_flag`) values('1774260','1','5483.24','-2351.26','1462.17','0','0','0','100','0','0','0');
+
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127427','123742','3','280','0');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127427','127427','0','0','0');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127427','123744','5','0','0');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127427','123747','5','330','0');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127427','123750','5','45','0');
+
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127506','124575','3','280','515');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127506','127506','0','0','515');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127506','125054','5','0','515');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127506','125069','5','330','515');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127506','125071','5','45','515');
+
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127577','125075','3','280','515');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127577','127577','0','0','515');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127577','125080','5','0','515');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127577','125094','5','330','515');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127577','125103','5','45','515');
+
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127529','125112','5','0','0');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127529','125521','5','330','0');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127529','127529','5','0','0');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127529','126370','5','45','0');
+
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127498','126381','3','280','0');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127498','127498','0','0','0');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127498','126642','5','0','0');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127498','126751','5','330','0');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127498','126796','5','45','0');
+
+
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('110014','126820','3','280','0');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('110014','110014','0','0','0');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('110014','126872','5','0','0');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('110014','126878','5','330','0');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('110014','126886','5','45','0');
+
+
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('109910','127243','3','280','515');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('109910','109910','0','0','515');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('109910','127373','5','0','515');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('109910','127380','5','330','515');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('109910','127387','5','45','515');
+
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('109827','127397','3','280','515');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('109827','109827','0','0','515');
+
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('109764','127407','3','280','515');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('109764','109764','0','0','515');
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('109764','127415','5','0','515');
+
+insert into `creature_formations` (`leaderGUID`, `memberGUID`, `dist`, `angle`, `groupAI`) values('127445','127445','0','0','515');
+
+
+
+
+
